2567816cb585d723d9c57a795fd13a391e80f6bd
Ismaël Sow <ismael@userstand.com>
2019-06-14 23:48:28 +0200

    Placeholder homepage

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+#
+custom_fonts = ["museo-sans-100.woff2","museo-sans-300.woff2", "museo-sans-700.woff2"]
+custom_css = ["core.css.scss", "static_pages.css"]
+
+Rails.application.config.assets.precompile += custom_fonts
+Rails.application.config.assets.precompile += custom_css
