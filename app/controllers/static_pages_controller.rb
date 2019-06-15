a2c7acf57d02cf807a03786bf603aa8499a8a6e9
Ismaël Sow <ismael@userstand.com>
2019-06-15 19:24:14 +0200

    Switch to static html file with bootstrap

--- app/controllers/static_pages_controller.rb
+++
@@ -1,8 +0,0 @@
- class StaticPagesController < ApplicationController
-   include ApplicationHelper
-
-   def coming_soon
-     @title = "Searchness: usability testing software"
-     render layout: "static_pages"
-   end
- end
